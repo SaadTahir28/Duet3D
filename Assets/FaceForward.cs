995d94c0f4b03e04ce53a3a76e3eac2baa757dbc
SaadTahir28 <saadtahir28@gmail.com>
2018-10-18 00:16:47 +0500

    Basic game functionality

---
+++ Assets/FaceForward.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceForward : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newRotation = Vector3.down * transform.parent.rotation.y;
+        transform.eulerAngles = newRotation;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float screenCenterX;
+
+    public float rotateSpeed = 100f;
+
+    private void Start()
+    {
+        screenCenterX = Screen.width * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            RotateClockwise();
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            RotateAntiClockwise();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x > screenCenterX)
+            {
+                RotateClockwise();
+            }
+            else if (Input.mousePosition.x < screenCenterX)
+            {
+                RotateAntiClockwise();
+            }
+        }
+	}
+
+    private void RotateClockwise()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+    }
+
+    private void RotateAntiClockwise(){
+        transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+    }
+
+   
+}
